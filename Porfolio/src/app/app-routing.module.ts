297f2bd080fb59118d4e89997e78577adda2826f
HAJITH-M <hajithm2003@gmail.com>
2023-10-15 15:15:08 +0530

    updated portfolio totally changed

--- Porfolio/src/app/app-routing.module.ts
+++ Porfolio/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  { path:'about', component: AboutComponent},
+  { path:'contact', component: ContactComponent},
+  { path:'skills', component: SkillsComponent},
+  { path:'portfolio', component: PortfolioComponent},
+  { path:'**', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
